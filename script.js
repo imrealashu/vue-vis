45c7a9ad626aa0ce13502f1e151b4fec14ac9840
Hitoshi Kuroyanagi <hitoshi.kuroyanagi@gmail.com>
2017-03-19 01:34:55 +0900

    feature reusable component
    We made multiple components at the same time.

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 var Vue = new Vue({
-	el: 'body',
+	el: '#app',
+	data: {
+		vueState: {
+			data1: [{ id: 1, content: 'item 1', start: '2013-04-20' },
+			{ id: 2, content: 'item 2', start: '2013-04-14' },
+			{ id: 3, content: 'item 3', start: '2013-04-18' },
+			{ id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19' },
+			{ id: 5, content: 'item 5', start: '2013-04-25' },
+			{ id: 6, content: 'item 6', start: '2013-04-27' }],
+
+			data2: [{ id: 1, content: 'item 11', start: '2017-04-20' },
+			{ id: 2, content: 'item 12', start: '2017-04-14' },
+			{ id: 3, content: 'item 13', start: '2017-04-18' },
+			{ id: 4, content: 'item 14', start: '2017-04-16', end: '2017-04-19' },
+			{ id: 5, content: 'item 15', start: '2017-04-25' },
+			{ id: 6, content: 'item 16', start: '2017-04-27' }]
+		}
+	}
 })
